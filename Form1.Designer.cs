ee6b46ac759b975c133249269b068171e6edec4a
Alexandre MERIGNAC <muse_boy@hotmail.fr>
2011-10-15 23:16:38 +0200

    Insertion du projet

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Generateur_MCD_MLD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button.Location = new System.Drawing.Point(24, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(165, 209);
+            this.button.TabIndex = 0;
+            this.button.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.button.Click += new System.EventHandler(this.panel1_Click);
+            this.button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(727, 438);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Generateur_MCD_MLD
+{
+    public partial class Form1 : Form
+    {
+        Point Start;
+        Point Fin;
+        Boolean clic;
+        ArrayList tabpoint = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool _mouseDownOverButton;
+
+        // Indique si le boutton est en train d'être déplacé
+        private bool _buttonIsMoving;
+
+        // Contient les coordonnées de la souris par rapport au coin supérieur gauche du boutton
+        private Point _cursOnButton;
+     
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_mouseDownOverButton && !_buttonIsMoving)
+            {
+                _buttonIsMoving = true;
+                _cursOnButton = new Point(e.X, e.Y);
+                button.BringToFront();
+            }
+            else if (_buttonIsMoving)
+            {
+                afficherleslignes();
+                Point point = PointToClient(MousePosition);
+                button.Left = point.X - _cursOnButton.X;
+                button.Top = point.Y - _cursOnButton.Y;
+            }
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            if (_buttonIsMoving)
+                return;
+
+            // Traitement à effectuer lors d'un clique sur le boutton
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseDownOverButton = (e.Button == MouseButtons.Left);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _mouseDownOverButton = false;
+            _buttonIsMoving = false;
+        }
+
+      
+        private void Form1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        public void afficherligne() {
+            Graphics NewGraphic = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Black, 2);
+            NewGraphic.DrawLine(myPen, Start, Fin);
+            Ligne ligne = new Ligne();
+            ligne.pens = myPen;
+            ligne.Starts = Start;
+            ligne.Fins = Fin;
+            tabpoint.Add(ligne);
+            Start = new Point(0, 0);
+            Fin = new Point(0, 0);
+        }
+        public void afficherleslignes()
+        {
+            foreach (Ligne ligne in tabpoint)
+            {
+                NewGraphic.DrawLine(ligne.pens, ligne.Starts, ligne.Fins);
+            }
+
+        }
+        public struct Ligne
+        {
+            public Pen pens;
+            public Point Starts;
+            public Point Fins;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            if (Start.X == 0)
+            {
+                clic = true;
+                Start = new Point(e.X, e.Y);
+             }
+            else {
+                clic = false;
+                Fin = new Point(e.X, e.Y);
+                afficherligne();
+            }
+           
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clic == true)
+            {
+                NewGraphic.Clear(Color.LightBlue);
+                afficherleslignes();
+                Pen myPen = new Pen(System.Drawing.Color.Black, 2);
+                NewGraphic.DrawLine(myPen, Start, new Point(e.X, e.Y));
+                
+            }
+        }
+         Graphics NewGraphic;
+            
+        private void Form1_Load(object sender, EventArgs e)
+         {
+             NewGraphic = this.CreateGraphics();
+            clic = false;
+        }
+
+   
+
+    }
+}
