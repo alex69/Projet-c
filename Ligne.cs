377b7daafb69d1ea9f8af5d622e8c9a860585404
Alexandre MERIGNAC <muse_boy@hotmail.fr>
2011-10-17 10:24:32 +0200

    ligne.cs ajouté au projet

---
+++ Ligne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Generateur_MCD_MLD
+{
+    class Ligne
+    {
+            public Pen pens;
+            public Point Starts;
+            public Point Fins;
+    }
+}
