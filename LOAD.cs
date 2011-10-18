3324a67e5b4b52f7a855c55a8755e3ed99795c10
Alexandre Vallet <alex0122@free.fr>
2011-10-18 18:47:39 +0200

    Pour poupounet le grand gay

---
+++ LOAD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generateur_MCD_MLD
+{
+    public class LOAD
+    {
+        public string test;
+    }
+}
