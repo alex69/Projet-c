5dfe05b9ece6fcc6088987eb08ab64d0b2bce0dc
Alexandre MERIGNAC <muse_boy@hotmail.fr>
2011-10-18 15:47:49 +0200

    oh yeah

---
+++ Dico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Generateur_MCD_MLD
+{
+    public partial class Dico : Form
+    {
+        public Dico()
+        {
+            InitializeComponent();
+        }
+    }
+}
