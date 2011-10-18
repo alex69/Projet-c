6619550948752a9dc0de1fd8cdb6476ec5f51922
Alexandre Vallet <alex0122@free.fr>
2011-10-18 13:54:48 +0200

    Ajout Taille

--- Table.cs
+++ Table.cs
@@ -12,6 +12,7 @@ namespace Generateur_MCD_MLD
 {
     class Table
     {
+        public int taille { get; set; }
         public string nom { get; set; }
         public ArrayList listepoints { get; set; }
         public ArrayList listelignes { get; set; }
