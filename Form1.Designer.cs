95b08f4efd53994181920aaaf4af299b64bc8ffa
Alexandre MERIGNAC <muse_boy@hotmail.fr>
2011-10-19 15:50:54 +0200

    qq modifs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Table = new System.Windows.Forms.Panel();
             this.compteur = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
@@ -36,12 +37,14 @@
             // 
             // Table
             // 
+            this.Table.AccessibleDescription = null;
+            this.Table.AccessibleName = null;
+            resources.ApplyResources(this.Table, "Table");
             this.Table.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Table.BackgroundImage = null;
             this.Table.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Table.Location = new System.Drawing.Point(106, 100);
+            this.Table.Font = null;
             this.Table.Name = "Table";
-            this.Table.Size = new System.Drawing.Size(165, 209);
-            this.Table.TabIndex = 0;
             this.Table.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
             this.Table.Click += new System.EventHandler(this.Table_Click);
             this.Table.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
@@ -49,44 +52,50 @@
             // 
             // compteur
             // 
-            this.compteur.AutoSize = true;
-            this.compteur.Location = new System.Drawing.Point(519, 9);
+            this.compteur.AccessibleDescription = null;
+            this.compteur.AccessibleName = null;
+            resources.ApplyResources(this.compteur, "compteur");
+            this.compteur.Font = null;
             this.compteur.Name = "compteur";
-            this.compteur.Size = new System.Drawing.Size(95, 13);
-            this.compteur.TabIndex = 1;
-            this.compteur.Text = "Nombre de lignes :";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.AccessibleDescription = null;
+            this.button1.AccessibleName = null;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackgroundImage = null;
+            this.button1.Font = null;
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Je serialise ma class LOAD";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(171, 9);
+            this.button2.AccessibleDescription = null;
+            this.button2.AccessibleName = null;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.BackgroundImage = null;
+            this.button2.Font = null;
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(202, 32);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Je deserialise mon fichier XML";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(727, 438);
+            this.BackColor = System.Drawing.Color.Azure;
+            this.BackgroundImage = null;
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.compteur);
             this.Controls.Add(this.Table);
+            this.Font = null;
+            this.Icon = null;
             this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
